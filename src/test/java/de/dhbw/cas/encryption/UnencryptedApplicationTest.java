a55c041882ffd3b8591999388d8af1d8dd5eacb2
sebastianBoehringer <45266845+sebastianBoehringer@users.noreply.github.com>
2024-12-24 15:40:03 +0100

    better assertion for smoke test

--- src/test/java/de/dhbw/cas/encryption/UnencryptedApplicationTest.java
+++ src/test/java/de/dhbw/cas/encryption/UnencryptedApplicationTest.java
@@ -1,16 +1,21 @@
 package de.dhbw.cas.encryption;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
 @TestPropertySource("classpath:properties/unencrypted.properties")
 class UnencryptedApplicationTest {
 
+    /**
+     * Tests that the application starts up when all properties are unencrypted
+     */
     @Test
-    void test_applicationStartsWithUnencryptedProperties() {
-        //This tests that the application in starts if all properties are unencrypted
+    void test_applicationStartsWithUnencryptedProperties(ApplicationContext context) {
+        Assertions.assertThat(context).isNotNull();
     }
 
 }
