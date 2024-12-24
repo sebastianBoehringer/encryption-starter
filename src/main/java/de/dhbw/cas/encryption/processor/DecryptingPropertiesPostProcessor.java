dd3cb79d5e28da2a547ecc3741e657e47cc517fa
sebastianBoehringer <45266845+sebastianBoehringer@users.noreply.github.com>
2024-12-24 14:01:56 +0100

    processor can handle symmetric ciphers

--- src/main/java/de/dhbw/cas/encryption/processor/DecryptingPropertiesPostProcessor.java
+++ src/main/java/de/dhbw/cas/encryption/processor/DecryptingPropertiesPostProcessor.java
@@ -1,13 +1,21 @@
 package de.dhbw.cas.encryption.processor;
 
+import de.dhbw.cas.encryption.configuration.DecryptionConfiguration;
+import de.dhbw.cas.encryption.decryptors.SymmetricDecryptor;
+import de.dhbw.cas.encryption.decryptors.TextDecryptor;
+import de.dhbw.cas.encryption.exception.DecryptionException;
+import de.dhbw.cas.encryption.util.HexConverter;
 import org.apache.commons.logging.Log;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.env.EnvironmentPostProcessor;
 import org.springframework.boot.logging.DeferredLogFactory;
 import org.springframework.core.annotation.Order;
 import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.Environment;
 import org.springframework.core.env.MapPropertySource;
 
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -30,13 +38,48 @@ public class DecryptingPropertiesPostProcessor implements EnvironmentPostProcess
 
     @Override
     public void postProcessEnvironment(ConfigurableEnvironment environment, SpringApplication application) {
-        log.info("Hurra I was called");
-        environment.getPropertySources().addFirst(createDummySource());
+        try {
+
+            log.debug("Trying to parse configuration");
+            DecryptionConfiguration configuration = DecryptionConfiguration.fromEnvironment(environment);
+            log.debug("Successfully parsed configuration [" + configuration + "]. Creating decryptor");
+            TextDecryptor decryptor;
+            if (configuration.symmetric()) {
+                decryptor = new SymmetricDecryptor(configuration.algorithm(), configuration.keyFile());
+                log.debug("Successfully created symmetric decryptor [" + decryptor + "]");
+            } else {
+                //TODO this will be implemented soon
+                return;
+            }
+
+            log.debug("Starting to decrypt properties");
+            Map<String, Object> decryptedProperties = getDecryptedProperties(environment, configuration, decryptor);
+            environment.getPropertySources().addFirst(new MapPropertySource(DECRYPTED_PROPERTY_SOURCE_NAME, decryptedProperties));
+            log.debug("Successfully added new property source to environment");
+        } catch (DecryptionException e) {
+            log.warn("Failed to decrypt properties", e);
+        }
     }
 
-    private MapPropertySource createDummySource() {
-        MapPropertySource source = new MapPropertySource(DECRYPTED_PROPERTY_SOURCE_NAME,
-                Map.of("spring.datasource.password", "bananenkey"));
-        return source;
+    private Map<String, Object> getDecryptedProperties(Environment environment, DecryptionConfiguration configuration, TextDecryptor decryptor) {
+        Map<String, Object> propertiesToDecrypt = new HashMap<>();
+        for (String property : configuration.properties()) {
+            String value = environment.getProperty(property);
+            if (value != null) {
+                try {
+                    String decryptedProperty = decryptor.decrypt(HexConverter.loadBytesFromHexString(value),
+                            new byte[0], StandardCharsets.US_ASCII);
+                    log.debug("Decrypted value [" + value + "] of property " + property + " to string of length " +
+                            decryptedProperty.length());
+                    propertiesToDecrypt.put(property, decryptedProperty);
+                } catch (DecryptionException e) {
+                    log.warn("Failed to decrypt property [" + property + "]", e);
+                }
+
+            } else {
+                log.debug("Skipping property [" + property + "] as its value is null");
+            }
+        }
+        return propertiesToDecrypt;
     }
 }
