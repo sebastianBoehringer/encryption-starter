ba173d71e924661bfb960675b7b87475fd254183
sebastianBoehringer <45266845+sebastianBoehringer@users.noreply.github.com>
2024-12-24 10:53:59 +0100

    add documentation to processor

--- src/main/java/de/dhbw/cas/encryption/processor/DecryptingPropertiesPostProcessor.java
+++ src/main/java/de/dhbw/cas/encryption/processor/DecryptingPropertiesPostProcessor.java
@@ -10,6 +10,15 @@ import org.springframework.core.env.MapPropertySource;
 
 import java.util.Map;
 
+/**
+ * This class decrypts marked properties.
+ * This allows users to include secrets in their configuration files without sharing them with the world. The processor
+ * works by decrypting the properties and providing the application with another property source that includes the
+ * decrypted properties. The name of the source is {@link #DECRYPTED_PROPERTY_SOURCE_NAME}. As that source is first
+ * Spring consults it first for properties. Thus, the decrypted properties are properly passed on to dependant
+ * frameworks like e.g. Hibernate.
+ * The processor can be configured via properties. See {@link de.dhbw.cas.encryption.configuration.DecryptionConfiguration}
+ */
 @Order
 public class DecryptingPropertiesPostProcessor implements EnvironmentPostProcessor {
     public static final String DECRYPTED_PROPERTY_SOURCE_NAME = "decrypted_properties";
