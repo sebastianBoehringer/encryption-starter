fb0d90e249954b1e831deda8106475831b727eb4
sebastianBoehringer <45266845+sebastianBoehringer@users.noreply.github.com>
2024-12-25 11:13:26 +0100

    implement test for aes in cbc mode to test iv usage

---
+++ src/test/java/de/dhbw/cas/encryption/AesCbcApplicationTest.java
@@ -0,0 +1,20 @@
+package de.dhbw.cas.encryption;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.TestPropertySource;
+
+@SpringBootTest
+@TestPropertySource("classpath:properties/aes-cbc.properties")
+class AesCbcApplicationTest {
+
+    /**
+     * Tests that the application starts up when decryption with AES/CBC is configured
+     */
+    @Test
+    void test_applicationStartsWithCorrectlyConfiguredAesCbcEncryption(ApplicationContext context) {
+        Assertions.assertThat(context).isNotNull();
+    }
+}
