a290948ec5d12d08614027d74cda92f680ef0a83
sebastianBoehringer <45266845+sebastianBoehringer@users.noreply.github.com>
2024-12-26 15:24:39 +0100

    add test for the GOST block cipher, add test that explicitly checks for decryption of multiple properties

--- processor/src/test/java/de/dhbw/cas/encryption/processor/DecryptingPropertiesPostProcessorTest.java
+++ processor/src/test/java/de/dhbw/cas/encryption/processor/DecryptingPropertiesPostProcessorTest.java
@@ -16,8 +16,18 @@ import java.util.Properties;
 
 class DecryptingPropertiesPostProcessorTest {
 
-    private static final String DEFAULT_ENCRYPTED_PROPERTY_NAME = "spring.datasource.password";
-    private static final String DEFAULT_ENCRYPTED_PROPERTY_DECRYPTED_VALUE = "root";
+    private static final String PASSWORD_PROPERTY = "spring.datasource.password";
+    private static final String DECRYPTED_PASSWORD_PROPERTY_VALUE = "root";
+
+    private static final String URL_PROPERTY = "spring.datasource.url";
+    private static final String DECRYPTED_URL_PROPERTY_VALUE = "jdbc:mariadb://localhost:3306/test";
+
+    private static final String USERNAME_PROPERTY = "spring.datasource.username";
+    private static final String DECRYPTED_USERNAME_PROPERTY_VALUE = "root";
+
+    private static final String APPLICATION_NAME_PROPERTY = "spring.application.name";
+    private static final String DECRYPTED_APPLICATION_NAME_PROPERTY_VALUE = "encryption-starter";
+
     private final DecryptingPropertiesPostProcessor processor = new DecryptingPropertiesPostProcessor(new DeferredLogs());
 
     private MockEnvironment setupMockEnv(String propertyFileName) throws IOException {
@@ -31,46 +41,70 @@ class DecryptingPropertiesPostProcessorTest {
 
 
     @ParameterizedTest(name = "can decrypt using {0}")
-    @ValueSource(strings = {"aes.properties", "aes-cbc.properties", "des-ede.properties", "ecies.properties", "el-gamal.properties", "rsa.properties"})
+    @ValueSource(strings = {"aes.properties", "aes-cbc.properties", "des-ede.properties", "ecies.properties",
+            "el-gamal.properties", "rsa.properties", "gost.properties"})
     void test_postProcessEnvironment_canDecryptConfiguredPropertyWithMultipleDifferentAlgorithms(String propertyFileName) throws IOException {
         final MockEnvironment environment = setupMockEnv("properties/" + propertyFileName);
-        Assertions.assertThat(environment.getProperty(DEFAULT_ENCRYPTED_PROPERTY_NAME))
-                .isNotEqualTo(DEFAULT_ENCRYPTED_PROPERTY_DECRYPTED_VALUE);
+        Assertions.assertThat(environment.getProperty(PASSWORD_PROPERTY))
+                .isNotEqualTo(DECRYPTED_PASSWORD_PROPERTY_VALUE);
         // As we know the actual implementation does not rely on the second parameter we could also pass in null instead
         processor.postProcessEnvironment(environment, new SpringApplication());
 
-        Assertions.assertThat(environment.getProperty(DEFAULT_ENCRYPTED_PROPERTY_NAME))
-                .isEqualTo(DEFAULT_ENCRYPTED_PROPERTY_DECRYPTED_VALUE);
+        Assertions.assertThat(environment.getProperty(PASSWORD_PROPERTY))
+                .isEqualTo(DECRYPTED_PASSWORD_PROPERTY_VALUE);
     }
 
     @Test
     void test_postProcessEnvironment_doesNothingWhenDisabled() throws IOException {
         final MockEnvironment environment = setupMockEnv("properties/unencrypted.properties");
-        final String preProcessingValue = environment.getProperty(DEFAULT_ENCRYPTED_PROPERTY_NAME);
-        Assertions.assertThat(preProcessingValue).isNotEqualTo(DEFAULT_ENCRYPTED_PROPERTY_DECRYPTED_VALUE);
+        final String preProcessingValue = environment.getProperty(PASSWORD_PROPERTY);
+        Assertions.assertThat(preProcessingValue).isNotEqualTo(DECRYPTED_PASSWORD_PROPERTY_VALUE);
 
         processor.postProcessEnvironment(environment, new SpringApplication());
 
-        Assertions.assertThat(environment.getProperty(DEFAULT_ENCRYPTED_PROPERTY_NAME))
-                .isNotEqualTo(DEFAULT_ENCRYPTED_PROPERTY_DECRYPTED_VALUE);
-        Assertions.assertThat(environment.getProperty(DEFAULT_ENCRYPTED_PROPERTY_NAME)).isEqualTo(preProcessingValue);
+        Assertions.assertThat(environment.getProperty(PASSWORD_PROPERTY))
+                .isNotEqualTo(DECRYPTED_PASSWORD_PROPERTY_VALUE);
+        Assertions.assertThat(environment.getProperty(PASSWORD_PROPERTY)).isEqualTo(preProcessingValue);
     }
 
     @Test
     void test_postProcessEnvironment_doesNotFailWhenPropertyIsWronglyEncrypted() throws IOException {
         final MockEnvironment environment = setupMockEnv("properties/aes-with-invalid-second-encrypted-property.properties");
-        final String preProcessingValue = environment.getProperty("spring.datasource.username");
+        final String preProcessingValue = environment.getProperty(USERNAME_PROPERTY);
         processor.postProcessEnvironment(environment, new SpringApplication());
-        Assertions.assertThat(environment.getProperty(DEFAULT_ENCRYPTED_PROPERTY_NAME)).isEqualTo(DEFAULT_ENCRYPTED_PROPERTY_DECRYPTED_VALUE);
-        Assertions.assertThat(environment.getProperty("spring.datasource.username")).isEqualTo(preProcessingValue);
+        Assertions.assertThat(environment.getProperty(PASSWORD_PROPERTY)).isEqualTo(DECRYPTED_PASSWORD_PROPERTY_VALUE);
+        Assertions.assertThat(environment.getProperty(USERNAME_PROPERTY)).isEqualTo(preProcessingValue);
     }
 
     @Test
     void test_postProcessEnvironment_silentlySkipsMissingPropertiesThatShouldBeDecrypted() throws IOException {
         final MockEnvironment environment = setupMockEnv("properties/des-ede-with-additional-missing-property-to-decrypt.properties");
-        Assertions.assertThat(environment.getProperty("spring.datasource.username")).isNull();
+        Assertions.assertThat(environment.getProperty(USERNAME_PROPERTY)).isNull();
         processor.postProcessEnvironment(environment, new SpringApplication());
-        Assertions.assertThat(environment.getProperty(DEFAULT_ENCRYPTED_PROPERTY_NAME)).isEqualTo(DEFAULT_ENCRYPTED_PROPERTY_DECRYPTED_VALUE);
-        Assertions.assertThat(environment.getProperty("spring.datasource.username")).isNull();
+        Assertions.assertThat(environment.getProperty(PASSWORD_PROPERTY)).isEqualTo(DECRYPTED_PASSWORD_PROPERTY_VALUE);
+        Assertions.assertThat(environment.getProperty(USERNAME_PROPERTY)).isNull();
+    }
+
+    @Test
+    void test_postProcessEnvironment_correctlyDecryptsMultiplePropertiesIfConfigured() throws IOException {
+        final MockEnvironment environment = setupMockEnv("properties/gost.properties");
+        final String preprocessedUsername = environment.getProperty(USERNAME_PROPERTY);
+        final String preprocessedPassword = environment.getProperty(PASSWORD_PROPERTY);
+        final String preprocessedUrl = environment.getProperty(URL_PROPERTY);
+        final String preprocessedName = environment.getProperty(APPLICATION_NAME_PROPERTY);
+
+        processor.postProcessEnvironment(environment, new SpringApplication());
+
+        Assertions.assertThat(environment.getProperty(USERNAME_PROPERTY)).isNotEqualTo(preprocessedUsername);
+        Assertions.assertThat(environment.getProperty(USERNAME_PROPERTY)).isEqualTo(DECRYPTED_USERNAME_PROPERTY_VALUE);
+
+        Assertions.assertThat(environment.getProperty(PASSWORD_PROPERTY)).isNotEqualTo(preprocessedPassword);
+        Assertions.assertThat(environment.getProperty(PASSWORD_PROPERTY)).isEqualTo(DECRYPTED_PASSWORD_PROPERTY_VALUE);
+
+        Assertions.assertThat(environment.getProperty(URL_PROPERTY)).isNotEqualTo(preprocessedUrl);
+        Assertions.assertThat(environment.getProperty(URL_PROPERTY)).isEqualTo(DECRYPTED_URL_PROPERTY_VALUE);
+
+        Assertions.assertThat(environment.getProperty(APPLICATION_NAME_PROPERTY)).isNotEqualTo(preprocessedName);
+        Assertions.assertThat(environment.getProperty(APPLICATION_NAME_PROPERTY)).isEqualTo(DECRYPTED_APPLICATION_NAME_PROPERTY_VALUE);
     }
 }
