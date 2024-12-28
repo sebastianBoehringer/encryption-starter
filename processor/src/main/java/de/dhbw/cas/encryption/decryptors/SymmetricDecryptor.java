928ca662e2effc8037781f81be0840f3acfc5fb8
sebastianBoehringer <45266845+sebastianBoehringer@users.noreply.github.com>
2024-12-28 14:21:14 +0100

    ivs for gcm mode have to be passed via different class

--- processor/src/main/java/de/dhbw/cas/encryption/decryptors/SymmetricDecryptor.java
+++ processor/src/main/java/de/dhbw/cas/encryption/decryptors/SymmetricDecryptor.java
@@ -9,6 +9,7 @@ import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.GCMParameterSpec;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 import java.nio.charset.Charset;
@@ -16,9 +17,13 @@ import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
 import java.security.Key;
 import java.security.NoSuchAlgorithmException;
+import java.security.spec.AlgorithmParameterSpec;
+import java.security.spec.InvalidParameterSpecException;
+import java.util.regex.Pattern;
 
 @NullMarked
 public class SymmetricDecryptor implements TextDecryptor {
+    private static final Pattern GCM_PATTERN = Pattern.compile("/GCM/");
     private final Key key;
     private final Cipher cipher;
 
@@ -44,13 +49,28 @@ public class SymmetricDecryptor implements TextDecryptor {
             if (iv == null || iv.length == 0) {
                 cipher.init(Cipher.DECRYPT_MODE, key);
             } else {
-                cipher.init(Cipher.DECRYPT_MODE, key, new IvParameterSpec(iv));
+                AlgorithmParameterSpec algorithmParameters = guessCorrectParameter(iv);
+                cipher.init(Cipher.DECRYPT_MODE, key, algorithmParameters);
             }
             byte[] decrypted = cipher.doFinal(encrypted);
             return new String(decrypted, charset);
         } catch (InvalidKeyException | IllegalBlockSizeException | BadPaddingException |
-                 InvalidAlgorithmParameterException e) {
+                 InvalidAlgorithmParameterException | InvalidParameterSpecException e) {
             throw new DecryptionException(e);
+        }
+    }
+
+    private AlgorithmParameterSpec guessCorrectParameter(byte[] iv) throws InvalidParameterSpecException {
+        if (cipher.getParameters() != null) {
+            return switch (cipher.getParameters().getParameterSpec(AlgorithmParameterSpec.class)) {
+                case GCMParameterSpec g -> new GCMParameterSpec(iv.length, iv);
+                default -> new IvParameterSpec(iv);
+            };
+        } else {
+            if (GCM_PATTERN.matcher(cipher.getAlgorithm()).find()) {
+                return new GCMParameterSpec(iv.length, iv);
+            }
+            return new IvParameterSpec(iv);
         }
     }
 }
