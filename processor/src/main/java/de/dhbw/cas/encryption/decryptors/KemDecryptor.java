50a102efb4435e4cf17af4f8ff4499f684592cf0
sebastianBoehringer <45266845+sebastianBoehringer@users.noreply.github.com>
2025-01-06 10:00:22 +0100

    add a decryptor for kem

---
+++ processor/src/main/java/de/dhbw/cas/encryption/decryptors/KemDecryptor.java
@@ -0,0 +1,38 @@
+package de.dhbw.cas.encryption.decryptors;
+
+import de.dhbw.cas.encryption.exception.DecryptionException;
+import de.dhbw.cas.encryption.util.AlgorithmUtil;
+import org.jspecify.annotations.NullMarked;
+import org.jspecify.annotations.Nullable;
+
+import javax.crypto.KEM;
+import javax.crypto.SecretKey;
+import java.nio.charset.Charset;
+import java.security.KeyFactory;
+import java.security.PrivateKey;
+import java.security.spec.PKCS8EncodedKeySpec;
+
+@NullMarked
+public class KemDecryptor implements TextDecryptor {
+
+    private final TextDecryptor delegate;
+
+    public KemDecryptor(final String kemTransformation, @Nullable final String keyAlgorithm,
+                        final byte[] encapsulatedKey, final byte[] kemKey) throws DecryptionException {
+        try {
+            final KeyFactory keyFactory = KeyFactory.getInstance(AlgorithmUtil.determineKeyAlgorithm(kemTransformation, keyAlgorithm));
+            final PrivateKey privateKey = keyFactory.generatePrivate(new PKCS8EncodedKeySpec(kemKey));
+            final KEM kem = KEM.getInstance(kemTransformation);
+            final KEM.Decapsulator decapsulator = kem.newDecapsulator(privateKey);
+            final SecretKey decapsulate = decapsulator.decapsulate(encapsulatedKey);
+            delegate = new SymmetricDecryptor(UnwrappingDecryptor.TRANSFORMATION_USED_WITH_UNWRAPPED_KEY, null, decapsulate.getEncoded());
+        } catch (Exception e) {
+            throw new DecryptionException(e);
+        }
+    }
+
+    @Override
+    public String decrypt(byte[] encrypted, byte @Nullable [] iv, Charset charset) throws DecryptionException {
+        return delegate.decrypt(encrypted, iv, charset);
+    }
+}
---
+++ processor/src/test/java/de/dhbw/cas/encryption/decryptors/KemDecryptorTest.java
@@ -0,0 +1,42 @@
+package de.dhbw.cas.encryption.decryptors;
+
+
+import de.dhbw.cas.encryption.exception.DecryptionException;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import javax.crypto.*;
+import javax.crypto.spec.IvParameterSpec;
+import java.nio.charset.StandardCharsets;
+import java.security.*;
+
+class KemDecryptorTest {
+
+    final String KEY_PAIR_ALGO = "X25519";
+
+    private KeyPair generateX25519KeyPair() throws NoSuchAlgorithmException {
+        final KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance(KEY_PAIR_ALGO);
+        return keyPairGenerator.generateKeyPair();
+    }
+
+    @Test
+    void test_decrypt_worksForDhKem() throws NoSuchAlgorithmException, InvalidKeyException, NoSuchPaddingException, InvalidAlgorithmParameterException, IllegalBlockSizeException, BadPaddingException, DecryptionException {
+        final String kemTransformation = "DHKEM";
+
+        final KeyPair kp = generateX25519KeyPair();
+        final KEM dhKem = KEM.getInstance(kemTransformation);
+        final KEM.Encapsulator encapsulator = dhKem.newEncapsulator(kp.getPublic());
+        final KEM.Encapsulated encapsulated = encapsulator.encapsulate(0, 32, "AES");
+
+        final SecretKey secretKey = encapsulated.key();
+        final Cipher aes = Cipher.getInstance(UnwrappingDecryptor.TRANSFORMATION_USED_WITH_UNWRAPPED_KEY);
+        final IvParameterSpec iv = new IvParameterSpec(new byte[aes.getBlockSize()]);
+        aes.init(Cipher.ENCRYPT_MODE, secretKey, iv);
+        String message = "KEM makes this go post quantum";
+        final byte[] encrypted = aes.doFinal(message.getBytes(StandardCharsets.UTF_8));
+
+        TextDecryptor decryptor = new KemDecryptor(kemTransformation, KEY_PAIR_ALGO, encapsulated.encapsulation(), kp.getPrivate().getEncoded());
+        Assertions.assertThat(decryptor.decrypt(encrypted, iv.getIV(), StandardCharsets.UTF_8)).isEqualTo(message);
+    }
+
+}
