13d830da672ade2f2270c86138ab0919de8e6d7c
sebastianBoehringer <45266845+sebastianBoehringer@users.noreply.github.com>
2025-01-06 10:38:44 +0100

    add test for ml-kem

--- processor/src/test/java/de/dhbw/cas/encryption/decryptors/KemDecryptorTest.java
+++ processor/src/test/java/de/dhbw/cas/encryption/decryptors/KemDecryptorTest.java
@@ -3,6 +3,7 @@ package de.dhbw.cas.encryption.decryptors;
 
 import de.dhbw.cas.encryption.exception.DecryptionException;
 import org.assertj.core.api.Assertions;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.junit.jupiter.api.Test;
 
 import javax.crypto.*;
@@ -32,10 +33,33 @@ class KemDecryptorTest {
         final Cipher aes = Cipher.getInstance(UnwrappingDecryptor.TRANSFORMATION_USED_WITH_UNWRAPPED_KEY);
         final IvParameterSpec iv = new IvParameterSpec(new byte[aes.getBlockSize()]);
         aes.init(Cipher.ENCRYPT_MODE, secretKey, iv);
-        String message = "KEM makes this go post quantum";
+        final String message = "KEM makes this go post quantum";
         final byte[] encrypted = aes.doFinal(message.getBytes(StandardCharsets.UTF_8));
 
         TextDecryptor decryptor = new KemDecryptor(kemTransformation, KEY_PAIR_ALGO, encapsulated.encapsulation(), kp.getPrivate().getEncoded());
+        Assertions.assertThat(decryptor.decrypt(encrypted, iv.getIV(), StandardCharsets.UTF_8)).isEqualTo(message);
+    }
+
+    @Test
+    void test_decrypt_worksForMlKem() throws NoSuchAlgorithmException, InvalidKeyException, NoSuchPaddingException, InvalidAlgorithmParameterException, IllegalBlockSizeException, BadPaddingException, DecryptionException {
+        Security.addProvider(new BouncyCastleProvider());
+
+        final String kemTransformation = "ML-KEM";
+        final KeyPairGenerator kpg = KeyPairGenerator.getInstance(kemTransformation);
+
+        final KeyPair kp = kpg.generateKeyPair();
+        final KEM dhKem = KEM.getInstance(kemTransformation);
+        final KEM.Encapsulator encapsulator = dhKem.newEncapsulator(kp.getPublic());
+        final KEM.Encapsulated encapsulated = encapsulator.encapsulate(0, 32, "AES");
+
+        final SecretKey secretKey = encapsulated.key();
+        final Cipher aes = Cipher.getInstance(UnwrappingDecryptor.TRANSFORMATION_USED_WITH_UNWRAPPED_KEY);
+        final IvParameterSpec iv = new IvParameterSpec(new byte[aes.getBlockSize()]);
+        aes.init(Cipher.ENCRYPT_MODE, secretKey, iv);
+        final String message = "Another KEM algorithm is actually supported!👌👌👌👌";
+        final byte[] encrypted = aes.doFinal(message.getBytes(StandardCharsets.UTF_8));
+
+        TextDecryptor decryptor = new KemDecryptor(kemTransformation, null, encapsulated.encapsulation(), kp.getPrivate().getEncoded());
         Assertions.assertThat(decryptor.decrypt(encrypted, iv.getIV(), StandardCharsets.UTF_8)).isEqualTo(message);
     }
 
